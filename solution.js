d6c9e3aeb1f20b4ea481a960f3822baabc14e76c
SwapnilYC <119727525+SwapnilYC@users.noreply.github.com>
2023-02-07 12:41:24 +0530

    an array is assigned as perinstructions

--- solution.js
+++ solution.js
@@ -2,6 +2,7 @@
 let yourArray;
 // assign your array the correct value in the next line
 // your code here!
+yourArray = ['one', 2, true, undefined, null];
 // code end
 
 module.exports = yourArray
